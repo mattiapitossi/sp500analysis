420ba9399d547d4a6b842f31b6d1fce5551184c5
michelafarruggia <michelafarruggia@icloud.com>
2023-05-15 19:03:47 +0200

    add plot top and worst performers

--- top_worst_performers.r
+++ top_worst_performers.r
@@ -78,7 +78,7 @@ mgdata_worst <- sp500 %>%
   summarise(avgClose = mean(Close))
 
 plot_worst <- ggplot(mgdata_worst, aes(Date, avgClose, group = 1)) +
-  geom_line() +
+  geom_line(color = "blue") +
   facet_wrap(~Symbol, nrow = 2)
 
 ggsave("worst_10_performing_stocks.png", plot_worst, width = 12, height = 6)
@@ -91,8 +91,38 @@ mgdata_top <- sp500 %>%
   summarise(avgClose = mean(Close))
 
 plot_top <- ggplot(mgdata_top, aes(Date, avgClose, group = 1)) +
-  geom_line() +
+  geom_line(color = "blue") +
   facet_wrap(~Symbol, nrow = 2)
 
 ggsave("top_10_performing_stocks.png", plot_top, width = 12, height = 6)
 print(plot_top)
+
+
+# Print top performer
+plot_data_top <- sp500 %>%
+  filter(Symbol == top_performer)
+
+plot_title <- paste0(top_performer, " Performance over the last 10 years")
+plot_filename <- paste0(top_performer, "_plot.png")
+
+plot_top_performer <- ggplot(plot_data_top, aes(x = Date, y = Pct_Change)) +
+  geom_line(color = "blue") +
+  labs(title = plot_title, x = "Date", y = "avgClose") +
+  ggtitle(plot_data_top$Symbol, "Top Performer Plot")
+
+ggsave("top_performing_stock.png", plot_top_performer, width = 12, height = 6)
+
+
+# Print worst performer
+plot_data_worst <- sp500 %>%
+  filter(Symbol == worst_performer)
+
+plot_title <- paste0(worst_performer, " Performance over the last 10 years")
+plot_filename <- paste0(worst_performer, "_plot.png")
+
+plot_worst_performer <- ggplot(plot_data_worst, aes(x = Date, y = Pct_Change)) +
+  geom_line(color = "blue") +
+  labs(title = plot_title, x = "Date", y = "avgClose") +
+  ggtitle(plot_data_worst$Symbol, "Worst Performer Plot")
+
+ggsave("worst_performing_stock.png", plot_worst_performer, width = 12, height = 6)
